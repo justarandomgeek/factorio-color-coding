3f96ca1d6e35ff6997f876e70087cc0a5b58dd03
Credomane Evonguard <github@credomane.com>
2016-07-08 23:00:11 -0500

    Update makerelease.sh
    * Using the -r option to output the raw value from jq.
    * Added quotes to git tag just-in-case.
    * Put brackets around modname so bash interprets is as "$modname" and not "$modname_".

--- makerelease.sh
+++ makerelease.sh
@@ -3,15 +3,11 @@
 
 # Get mod name and version from info.json
 # https://stedolan.github.io/jq/
-jsonname=`cat info.json|jq .name`
-jsonversion=`cat info.json|jq .version`
-
-#hack to unquote strings
-modname=$jsonname
-modver=$jsonversion
+modname=`cat info.json|jq -r .name`
+modver=`cat info.json|jq -r .version`
 
 # Create git tag for this version 
-git tag $modver
+git tag "$modver"
 
 # Prepare zip for Factorio native use and mod portal
-git archive --prefix "$modname_$modver/" -o "$modname_$modver.zip" HEAD
+git archive --prefix "${modname}_$modver/" -o "${modname}_$modver.zip" HEAD
