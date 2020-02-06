1661beb7ecad1c2e40f5c067d7afcc4bd657aab9
Thomas Deal <justarandomgeek@gmail.com>
2020-02-05 22:53:43 -0500

    most concrete now uses tint

--- graphics/make-concrete-sprites.sh
+++ graphics/make-concrete-sprites.sh
@@ -2,13 +2,5 @@
 
 C=15
 
-for f in $(ls -1 ./concrete/_source) ; do convert ./concrete/_source/$f -fill "rgb(255,0,0)"   -colorize $C,$C,$C,0 -level "0%,100%,0.8" ./concrete/red/$f      ; done
-for f in $(ls -1 ./concrete/_source) ; do convert ./concrete/_source/$f -fill "rgb(255,127,0)" -colorize $C,$C,$C,0 -level "0%,100%,0.8" ./concrete/orange/$f   ; done
-for f in $(ls -1 ./concrete/_source) ; do convert ./concrete/_source/$f -fill "rgb(255,255,0)" -colorize $C,$C,$C,0 -level "0%,100%,0.8" ./concrete/yellow/$f   ; done
-for f in $(ls -1 ./concrete/_source) ; do convert ./concrete/_source/$f -fill "rgb(0,255,0)"   -colorize $C,$C,$C,0 -level "0%,100%,0.8" ./concrete/green/$f    ; done
-for f in $(ls -1 ./concrete/_source) ; do convert ./concrete/_source/$f -fill "rgb(0,255,255)" -colorize $C,$C,$C,0 -level "0%,100%,0.8" ./concrete/cyan/$f     ; done
-for f in $(ls -1 ./concrete/_source) ; do convert ./concrete/_source/$f -fill "rgb(0,0,255)"   -colorize $C,$C,$C,0 -level "0%,100%,0.8" ./concrete/blue/$f     ; done
-for f in $(ls -1 ./concrete/_source) ; do convert ./concrete/_source/$f -fill "rgb(127,0,255)" -colorize $C,$C,$C,0 -level "0%,100%,0.8" ./concrete/purple/$f   ; done
-for f in $(ls -1 ./concrete/_source) ; do convert ./concrete/_source/$f -fill "rgb(255,0,255)" -colorize $C,$C,$C,0 -level "0%,100%,0.8" ./concrete/magenta/$f  ; done
-for f in $(ls -1 ./concrete/_source) ; do convert ./concrete/_source/$f -fill "rgb(255,127,0)"                      -level "0%,70%,1.2"  ./concrete/white/$f    ; done
-for f in $(ls -1 ./concrete/_source) ; do convert ./concrete/_source/$f -fill "rgb(0,0,0)"                          -level "0%,100%,0.4" ./concrete/black/$f    ; done
+for f in $(ls -1 ./concrete/_source) ; do convert ./concrete/_source/$f -fill "rgb(255,127,0)" -level "0%,70%,1.2"  ./concrete/white/$f ; done
+for f in $(ls -1 ./concrete/_source) ; do convert ./concrete/_source/$f -fill "rgb(0,0,0)"     -level "0%,100%,0.4" ./concrete/black/$f ; done
--- prototypes/tiles.lua
+++ prototypes/tiles.lua
@@ -7,8 +7,18 @@ function add_concrete(color,rgb)
     local concrete = util.table.deepcopy(data.raw["tile"]["hazard-concrete-left"])
     concrete.name = "concrete-"..color
     concrete.next_direction = nil
-    concrete.variants.material_background.picture = "__color-coding__/graphics/concrete/"..color.."/concrete.png"
-    concrete.variants.material_background.hr_version.picture = "__color-coding__/graphics/concrete/"..color.."/hr-concrete.png"
+    if color == "white" or color == "black" then
+        concrete.variants.material_background.picture = "__color-coding__/graphics/concrete/"..color.."/concrete.png"
+        concrete.variants.material_background.hr_version.picture = "__color-coding__/graphics/concrete/"..color.."/hr-concrete.png"
+    else
+        concrete.variants.material_background.picture = "__base__/graphics/terrain/concrete/concrete.png"
+        concrete.variants.material_background.hr_version.picture = "__base__/graphics/terrain/concrete/hr-concrete.png"
+        concrete.tint = {
+            r = ((rgb["r"]+1)/2),
+            g = ((rgb["g"]+1)/2),
+            b = ((rgb["b"]+1)/2)
+        }
+    end
     concrete.map_color = {
         r = (rgb["r"] * 0.5),
         g = (rgb["g"] * 0.5),
@@ -20,9 +30,20 @@ function add_concrete(color,rgb)
 
     local rconcrete = util.table.deepcopy(data.raw["tile"]["refined-hazard-concrete-left"])
     rconcrete.name = "refined-concrete-"..color
+    rconcrete.transition_merges_with_tile = "refined-concrete"
     rconcrete.next_direction = nil
-    rconcrete.variants.material_background.picture = "__color-coding__/graphics/concrete/"..color.."/refined-concrete.png"
-    rconcrete.variants.material_background.hr_version.picture = "__color-coding__/graphics/concrete/"..color.."/hr-refined-concrete.png"
+    if color == "white" or color == "black" then
+        rconcrete.variants.material_background.picture = "__color-coding__/graphics/concrete/" .. color .. "/refined-concrete.png"
+        rconcrete.variants.material_background.hr_version.picture = "__color-coding__/graphics/concrete/" .. color .. "/hr-refined-concrete.png"
+    else
+        rconcrete.variants.material_background.picture = "__base__/graphics/terrain/concrete/refined-concrete.png"
+        rconcrete.variants.material_background.hr_version.picture = "__base__/graphics/terrain/concrete/hr-refined-concrete.png"
+        rconcrete.tint = {
+            r = ((rgb["r"]+1)/2),
+            g = ((rgb["g"]+1)/2),
+            b = ((rgb["b"]+1)/2)
+        }
+    end
     rconcrete.map_color = {
         r = (rgb["r"] * 0.5),
         g = (rgb["g"] * 0.5),
